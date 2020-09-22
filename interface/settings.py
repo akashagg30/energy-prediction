a721cf2c552e8a016082b0dccf4409e77a1c2c57
akashagg30 <agarwal.akash30@gmail.com>
2020-09-23 05:04:12 +0530

    sqlite3

--- interface/settings.py
+++ interface/settings.py
@@ -80,11 +80,8 @@ WSGI_APPLICATION = 'interface.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Energy_Database',
-        'USER' : 'postgres',
-        'PASSWORD' : '1234',
-        'HOST' : 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
